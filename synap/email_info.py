48de59d08e0b4da71d5b161ffc82eef02ab64347
rajac123 <92913676+rajac123@users.noreply.github.com>
2024-03-30 13:43:34 +0530

    Update email_info.py

--- synap/email_info.py
+++ synap/email_info.py
@@ -4,6 +4,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'arunbabusynapstics@gmail.com'
-EMAIL_HOST_PASSWORD = 'Synap@123'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
